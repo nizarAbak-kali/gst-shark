8d107c1bb98966eec9e9bd0d22b6d1854760e959
Manuel <manuel.leiva@ridgerun.com>
2016-03-18 14:28:02 -0600

    Add test script

---
+++ plugins/tracers/test-ctf.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo "Compile concept api application" 
+gcc -o ctf_app ctf-api-test.c
+
+echo "Create matadata and datastream"
+./ctf_app;
+
+echo "Compare ref datastream and test datastream" 
+cp datastream metadata test
+./compare.sh
+
+echo "Read the CTF file generated with babeltrace" 
+babeltrace ./test
