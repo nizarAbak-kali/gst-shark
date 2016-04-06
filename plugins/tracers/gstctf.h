2b6c1e870b711a2b7657a1b2e95619910f08a960
Sebastian Fatjo <sebastian.fatjo@ridgerun.com>
2016-04-06 11:40:45 -0600

    Fixing typo on function declaration

--- plugins/tracers/gstctf.h
+++ plugins/tracers/gstctf.h
@@ -39,7 +39,7 @@ void gst_ctf_close ();
 void add_metadata_event_struct (const gchar *metadata_event, event_id id,
     gint stream_id);
 void do_print_cpuusage_event (event_id id, guint32 cpunum, guint64 cpuload);
-void do_print_proctime_event (event_id id, gchar *elementname, uint64_t time);
+void do_print_proctime_event (event_id id, gchar *elementname, guint64 time);
 void do_print_framerate_event (event_id id, gchar *padname, guint64 fps);
 void do_print_interlatency_event (event_id id,
     char *originpad, gchar *destinationpad, guint64 time);
