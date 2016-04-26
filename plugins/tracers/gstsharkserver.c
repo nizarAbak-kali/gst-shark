4987d152dd04e8f22dbe5e0581522c7e5c537c5f
Manuel <manuel.leiva@ridgerun.com>
2016-04-25 19:52:16 -0600

    Change gstsharkserver application to read the TCP header
    test: NO test

--- plugins/tracers/gstsharkserver.c
+++ plugins/tracers/gstsharkserver.c
@@ -1,20 +1,31 @@
 
 #include <stdlib.h>
+#include <stdint.h>
 #include <sys/stat.h>
  #include <stdio.h>
 
 #define DIR_NAME "./gst-shark-server/"
+
+#define TCP_METADATA_ID  (0x01)
+#define TCP_DATASTREAM_ID  (0x02)
+
+typedef uint8_t  tcp_header_id;
+typedef uint32_t tcp_header_length;
 
 int main (int argc, char * argv[])
 {
     FILE * fd;
     FILE * fd_metadata;
     FILE * fd_datastream;
-    struct stat st;
+    //~ struct stat st;
     int size;
-    int file_idx;
+    //~ int file_idx;
     char * file;
-    int metadata_size;
+    //~ int metadata_size;
+    tcp_header_id header_id;
+    int header_id_position;
+    tcp_header_length header_length;
+    int file_read;
     
     if (argc < 2)
     {
@@ -44,23 +55,6 @@ int main (int argc, char * argv[])
     
     fread(file,sizeof(char),size,fd);
     
-    metadata_size = size;
-    for (file_idx = 0; file_idx <= size; ++file_idx)
-    {
-        //~ printf("idx: %d %x\n",file_idx, (unsigned int)(0xFF &file[file_idx]));
-        if ((unsigned int)0xC1 ==(unsigned int) (0xFF &file[file_idx]))
-        {
-            metadata_size = file_idx;
-            //~ printf("HIT idx: %d\n",file_idx);
-            break;
-        }
-    } 
-    
-    if (stat(DIR_NAME, &st) == -1)
-    {
-        mkdir(DIR_NAME, 0775);
-    }
-    
     fd_metadata = fopen(DIR_NAME"/metadata","w");
     fd_datastream = fopen(DIR_NAME"/datastream","w");
     
@@ -69,8 +63,68 @@ int main (int argc, char * argv[])
         fprintf(stderr,"ERROR: metadata or datastream can not be opened\n");
     }
     
-    fwrite(file,sizeof(char),metadata_size,fd_metadata);
-    fwrite(&file[metadata_size],sizeof(char),size-metadata_size,fd_datastream);
-
+    file_read = 0;
+    while (file_read <= size)
+    {
+        printf("file_read %d\n",file_read);
+        header_id = file[file_read];
+        printf("file_read %d\n",file_read);
+        header_id_position = file_read;
+        printf("header_id_position %d\n",header_id_position);
+        file_read = sizeof(header_id);
+        header_length = *((tcp_header_length*)(&file[file_read]));
+        file_read =+ sizeof(tcp_header_length);
+        
+        printf("header_id: 0x%X header_length %d\n",header_id, header_length);
+        if (header_length < 0)
+        {
+            break;
+        }
+        switch (header_id)
+        {
+            case TCP_METADATA_ID:
+                fwrite(&file[file_read],sizeof(char),header_length,fd_metadata);
+                break;
+            case TCP_DATASTREAM_ID:
+                fwrite(&file[file_read],sizeof(char),header_length,fd_datastream);
+                break;
+            default:
+                fprintf(stderr,"ERROR: unknown TCP ID header [0x%X] at position %d\n",header_id,header_id_position);
+                return EXIT_SUCCESS;
+        }
+        file_read += header_length;
+        printf("file_read %d\n",file_read);
+    }
+    
+    fclose(fd_metadata);
+    fclose(fd_datastream);
+    //~ for (file_idx = 0; file_idx <= size; ++file_idx)
+    //~ {
+        //~ 
+        //~ printf("idx: %d %x\n",file_idx, (unsigned int)(0xFF &file[file_idx]));
+        //~ if ((unsigned int)0xC1 ==(unsigned int) (0xFF &file[file_idx]))
+        //~ {
+            //~ metadata_size = file_idx;
+            //~ printf("HIT idx: %d\n",file_idx);
+            //~ break;
+        //~ }
+    //~ } 
+    //~ 
+    //~ if (stat(DIR_NAME, &st) == -1)
+    //~ {
+        //~ mkdir(DIR_NAME, 0775);
+    //~ }
+    //~ 
+    //~ fd_metadata = fopen(DIR_NAME"/metadata","w");
+    //~ fd_datastream = fopen(DIR_NAME"/datastream","w");
+    //~ 
+    //~ if (NULL == fd_metadata || NULL == fd_datastream)
+    //~ {
+        //~ fprintf(stderr,"ERROR: metadata or datastream can not be opened\n");
+    //~ }
+    //~ 
+    //~ fwrite(file,sizeof(char),metadata_size,fd_metadata);
+    //~ fwrite(&file[metadata_size],sizeof(char),size-metadata_size,fd_datastream);
+//~ 
     return EXIT_SUCCESS;
 }
--- plugins/tracers/test_tcp_conn.sh
+++ plugins/tracers/test_tcp_conn.sh
@@ -7,13 +7,13 @@ sudo nc -l 8080 > metadata_tcp &
 sleep 1
 GST_DEBUG=3 GST_SHARK_TRACE_LOC="file://./gst-shark-test;tcp://localhost:8080" ./gstsharkclient 
 
-#~ ./gstsharkserver metadata_tcp
-#~ 
+./gstsharkserver metadata_tcp
+
 #~ sudo echo "Files produced"
 #~ ls -l metadata_tcp
 #~ ls -l ./gst-shark-server/
 #~ ls -l ./gst-shark-test
-#~ 
+
 #~ sudo echo "Test TCP"
 #~ babeltrace ./gst-shark-server
 #~ sudo echo "Test File"
