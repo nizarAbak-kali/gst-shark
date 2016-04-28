e8d6807d7014a6bc58441d39500e8f33084bf3d9
Manuel <manuel.leiva@ridgerun.com>
2016-04-27 18:23:22 -0600

    Add TCP header in function do_print_framerate_event.
    Test: ./test_tcp_conn.sh

--- plugins/tracers/gstctf.c
+++ plugins/tracers/gstctf.c
@@ -785,15 +785,57 @@ do_print_proctime_event (event_id id, gchar * elementname, guint64 time)
 void
 do_print_framerate_event (event_id id, const gchar * padname, guint64 fps)
 {
-  gint size = strlen (padname);
+  //~ gint size = strlen (padname);
+  GError * error;
+  guint8 * mem;
+  guint8 * payload;
+  guint8 * payload_end;
+  gsize payload_size;
+  guint event_header_size;
 
-  if (ctf_descriptor->file_output_disable)
-    return;
+  mem = ctf_descriptor->mem;
+  payload = mem + TCP_HEADER_SIZE;
+
 
   g_mutex_lock (&ctf_descriptor->mutex);
   //~ add_event_header (id);
-  fwrite (padname, sizeof (gchar), size + 1, ctf_descriptor->datastream);
-  fwrite (&fps, sizeof (gchar), sizeof (guint64), ctf_descriptor->datastream);
+    event_header_size = add_event_header (id, payload);
+  payload += event_header_size;
+  /* Write Pad name */
+    payload_end = (guint8 *)g_stpcpy ((gchar*)payload,padname);
+  *(gchar*)payload_end = '\0';
+  ++payload_end;
+  payload += payload_end - payload; 
+  /* Write FPS */
+  *(guint64*)payload = fps;
+  payload += sizeof(guint64);
+  
+  payload_size = payload - (mem + TCP_HEADER_SIZE);
+  
+  if (FALSE == ctf_descriptor->file_output_disable )
+  {
+    payload = mem + TCP_HEADER_SIZE;
+    fwrite (payload, sizeof (gchar), payload_size,
+      ctf_descriptor->datastream);
+  }
+
+  if (FALSE == ctf_descriptor->tcp_output_disable )
+  {
+    /* Write the TCP header */
+    *(tcp_header_id*)mem = TCP_DATASTREAM_ID;
+    mem += sizeof(tcp_header_id);
+    *(tcp_header_length*)mem = payload_size;
+    
+    g_output_stream_write  (ctf_descriptor->output_stream,
+                          ctf_descriptor->mem,
+                          payload_size + TCP_HEADER_SIZE,
+                          NULL,
+                          &error);
+  }
+   
+  
+  //~ fwrite (padname, sizeof (gchar), size + 1, ctf_descriptor->datastream);
+  //~ fwrite (&fps, sizeof (gchar), sizeof (guint64), ctf_descriptor->datastream);
   g_mutex_unlock (&ctf_descriptor->mutex);
 }
 
--- plugins/tracers/gstsharkclient.c
+++ plugins/tracers/gstsharkclient.c
@@ -26,6 +26,17 @@ static const char proctime_metadata_event[] = "event {\n\
 };\n\
 \n";
 
+static const char framerate_metadata_event[] = "event {\n\
+	name = framerate;\n\
+	id = %d;\n\
+	stream_id = %d;\n\
+	fields := struct {\n\
+		string padname;\n\
+		integer { size = 64; align = 8; signed = 0; encoding = none; base = 10; } _fps;\n\
+	};\n\
+};\n\
+\n";
+
 void tcp_conn_write(void);
 
 int main (int argc, char * argv[])
@@ -40,15 +51,22 @@ int main (int argc, char * argv[])
     add_metadata_event_struct (metadata_event);
     g_free (metadata_event);
     
-        metadata_event =
+    metadata_event =
     g_strdup_printf (proctime_metadata_event, PROCTIME_EVENT_ID, 0);
+    add_metadata_event_struct (metadata_event);
+    g_free (metadata_event);
+    
+    metadata_event =
+    g_strdup_printf (framerate_metadata_event, FPS_EVENT_ID, 0);
     add_metadata_event_struct (metadata_event);
     g_free (metadata_event);
 
     /* Events */
     do_print_cpuusage_event (CPUUSAGE_EVENT_ID, 0, 75);
     
-    do_print_proctime_event (PROCTIME_EVENT_ID, "identity", 100);
+    do_print_proctime_event (PROCTIME_EVENT_ID, "identity0", 128);
+    
+    do_print_framerate_event (FPS_EVENT_ID, "identity0_pad", 256);
 
     gst_ctf_close ();
 
