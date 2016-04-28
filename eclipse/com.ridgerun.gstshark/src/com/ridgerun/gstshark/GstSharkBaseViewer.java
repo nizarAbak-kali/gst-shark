e200c01e75d6572f36c977ee993ed81dc82bf650
Manuel <manuel.leiva@ridgerun.com>
2016-04-28 17:02:55 -0600

    Add framerate view

--- eclipse/com.ridgerun.gstshark/src/com/ridgerun/gstshark/GstSharkBaseViewer.java
+++ eclipse/com.ridgerun.gstshark/src/com/ridgerun/gstshark/GstSharkBaseViewer.java
@@ -17,18 +17,31 @@ import org.eclipse.tracecompass.tmf.ui.viewers.xycharts.linecharts.TmfCommonXLin
 public class GstSharkBaseViewer extends TmfCommonXLineChartViewer {
 
 	private ITmfTrace _trace = null;
+	private String _event = null;
+	private String _param1 = null;
+	private String _param2 = null;
+	private String _param3 = null;
 	
-	public GstSharkBaseViewer(Composite parent, String title, String xLabel, String yLabel) {
-		super(parent, title, xLabel, yLabel);
+	public GstSharkBaseViewer(Composite parent, String title, String xLabel, String yLabel, String event, String param1, String param2, String param3 ) {
+		super(parent, title, xLabel, yLabel );
+		_event = event;
+		_param1 = param1;
+		_param2 = param2;
+		_param3 = param3;
 	}
 	
 	private ITmfEvent getNextFilteredByName(ITmfContext ctx, String name) {
 		ITmfEvent event = null;
-		
+		System.out.print(String.format("Search event name: %s\n", name));
 		do {
 			event = _trace.getNext(ctx);
-		} while (null == event || !event.getName().equals(name) || !event.getContent().getField("elementname").getValue().equals("fakesink0_sink"));
-
+			
+			if (null != event)
+			{
+				//System.out.print(String.format("%s\n", event.getName()));
+			}
+		} while (null == event || !event.getName().equals(name) || !event.getContent().getField(_param1).getValue().equals(_param2));
+		
 		return event;
 	}
 	
@@ -38,13 +51,18 @@ public class GstSharkBaseViewer extends TmfCommonXLineChartViewer {
 		ITmfTimestamp endTimestamp = _trace.createTimestamp(end);
 		System.out.println(String.format("Range is %s to %s", startTimestamp.toString(), endTimestamp.toString()));
 		ITmfContext ctx = _trace.seekEvent(startTimestamp);
-		ITmfEvent event = getNextFilteredByName(ctx, "scheduling");
-
+		ITmfEvent event = getNextFilteredByName(ctx, _event);
+		
+		ITmfTimestamp ts = event.getTimestamp();
+		
 		while (null != event && 0 > event.getTimestamp().compareTo(endTimestamp)) {
 			events.add(event);
-			event = getNextFilteredByName(ctx, "scheduling");
+			event = getNextFilteredByName(ctx, _event);
 		}
-		System.out.println(String.format("Last ts %s - end %s", events.get(events.size()-1).getTimestamp(), endTimestamp));
+		if (events.size() > 0)
+		{
+		  System.out.println(String.format("Last ts %s - end %s", events.get(events.size()-1).getTimestamp(), endTimestamp));
+		}
 		
 		return events;
 	}
@@ -66,28 +84,35 @@ public class GstSharkBaseViewer extends TmfCommonXLineChartViewer {
 		System.out.println(getTimeOffset());
 		
 		
-		
-		
 		List <ITmfEvent> events = getEventsInRange(start, end);
+		
+		if (events.size() == 0)
+		{
+			System.out.println("**** Event Size = 0");
+			//updateDisplay();
+			return;
+		}
 		//events = resizeEventList (events, nb);
 		double xx[] = new double[events.size()];
 		double y[] = new double[events.size()];
 		System.out.println("===========================");
 		for (int i = 0; i < events.size(); ++i) {
 			xx[i] = events.get(i).getTimestamp().getValue() - getTimeOffset();
-			y[i] = new Double(events.get(i).getContent().getField("time").getValue().toString());
+			y[i] = new Double(events.get(i).getContent().getField(_param3).getValue().toString());
 
 			//System.out.print(String.format("%f - %f\n", x[i], y[i]));
 		}
-		double x[] = getXAxis(start, end, events.size());
-		System.out.println(String.format("%f - %f - %f", x[0], xx[0], y[0]));
+		//double x[] = getXAxis(start, end, events.size());
+		//System.out.println(String.format("%f - %f - %f", x[0], xx[0], y[0]));
 		//System.out.println(String.format("%f - %f - %f", x[x.length-1], xx[x.length-1], y[x.length-1]));
 
 		System.out.println("===========================");
 
 		//clearContent();
 		setXAxis(xx);
-		setSeries("test1", y);
+		
+		//setSeries(events.get(1).getName(), y);
+		setSeries(_param2,y);
 		updateDisplay();
 	}	
 
@@ -109,6 +134,6 @@ public class GstSharkBaseViewer extends TmfCommonXLineChartViewer {
 			}
 			event = _trace.getNext(context);
 		}
-		addSeries("test1");
+		//addSeries("test1");
     }
 }
