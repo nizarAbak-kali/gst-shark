c552e7dfd9dcb31efab64dbab4f773c274dcb3a4
Manuel <manuel.leiva@ridgerun.com>
2016-05-02 16:55:10 -0600

    Implement the TCP communication in do_print_interlatency_event

--- plugins/tracers/gstsharkclient.c
+++ plugins/tracers/gstsharkclient.c
@@ -37,6 +37,19 @@ static const char framerate_metadata_event[] = "event {\n\
 };\n\
 \n";
 
+
+static const char interlatency_metadata_event[] = "event {\n\
+	name = interlatency;\n\
+	id = %d;\n\
+	stream_id = %d;\n\
+	fields := struct {\n\
+		string src;\n\
+		string element;\n\
+		integer { size = 64; align = 8; signed = 0; encoding = none; base = 10; } _time;\n\
+	};\n\
+};\n\
+\n";
+
 void tcp_conn_write(void);
 
 int main (int argc, char * argv[])
@@ -61,12 +74,21 @@ int main (int argc, char * argv[])
     add_metadata_event_struct (metadata_event);
     g_free (metadata_event);
 
+    metadata_event =
+    g_strdup_printf (interlatency_metadata_event, INTERLATENCY_EVENT_ID, 0);
+    add_metadata_event_struct (metadata_event);
+    g_free (metadata_event);
+
+
     /* Events */
     do_print_cpuusage_event (CPUUSAGE_EVENT_ID, 0, 75);
     
     do_print_proctime_event (PROCTIME_EVENT_ID, "identity0", 128);
     
     do_print_framerate_event (FPS_EVENT_ID, "identity0_pad", 256);
+    
+
+    do_print_interlatency_event (INTERLATENCY_EVENT_ID, "originpad", "destinationpad", 512);
 
     gst_ctf_close ();
 
