0cfe380d081d4482316afdf0a132096afda42335
Manuel <manuel.leiva@ridgerun.com>
2016-06-16 14:34:38 -0600

    Add a plot for interlatency event

--- octave/plot_interlatency.m
+++ octave/plot_interlatency.m
@@ -2,6 +2,7 @@
 
 # Configuration
 RESULT = 1;
+FONTSIZE = 14;
 
 # Constants
 TRUE = 1;
@@ -13,11 +14,10 @@ fileID = fopen('interlatency.mat');
 count = 1;
 # Compute How many series need to be created
 
-[timestamp count] = fscanf(fileID,'[%s]');
+[timestamp1 count] = fscanf(fileID,'[%s]');
 [from_pad count] = fscanf(fileID,'%s\"');
 [to_pad count] = fscanf(fileID,'%s\"');
 [time count] = fscanf(fileID,'%d');
-printf('%s %s %s %d\n',timestamp,from_pad,to_pad,time);
 
 # Store the source element
 src_pad = from_pad;
@@ -40,7 +40,6 @@ while (count == 1)
             pad_found = TRUE;
         end
     end
-    %~ pad_found
     if (pad_found == FALSE)
         pad_name_list_len = length(pad_name_list) + 1;
         pad_name_list{pad_name_list_len} = to_pad;
@@ -48,7 +47,7 @@ while (count == 1)
     end
     pad_found = FALSE;
     
-    printf('%s %s %s %d\n',timestamp,from_pad,to_pad,time);
+    %~ printf('%s %s %s %d\n',timestamp,from_pad,to_pad,time);
 end
 
 if (RESULT)
@@ -56,5 +55,41 @@ if (RESULT)
         printf('%s %d \n',pad_name_list{list_idx},pad_freq_list(list_idx));
     end
 end
+
+# Creata matrix to store the data
+timestamp_mat = nan(length(pad_name_list),max(pad_freq_list));
+time_mat = nan(size(timestamp_mat));
+
+# Move to the beginning of a file
+frewind(fileID)
+data_mat_idx_list = ones(1,length(pad_name_list));
+pad_name_list_len = length(pad_name_list);
+count = 1;
+while (count == 1) 
+    [timestamp count] = fscanf(fileID,'[%s]');
+    if (count == 0)
+        break
+    end
+    [from_pad count] = fscanf(fileID,'%s\"');
+    [to_pad count] = fscanf(fileID,'%s\"');
+    [time count] = fscanf(fileID,'%d');
+    # Match the event with a pad name
+    for list_idx = 1:pad_name_list_len
+        if (1 == strcmp(char(pad_name_list{list_idx}),to_pad))
+            [timestamp_array] = sscanf(timestamp,'%d:%d:%f]');
+            timestamp_val = timestamp_array(3) + (timestamp_array(2) * 60) + timestamp_array(1);
+            timestamp_mat(list_idx,data_mat_idx_list(list_idx)) = timestamp_val;
+            time_mat(list_idx,data_mat_idx_list(list_idx)) = time;
+            data_mat_idx_list(list_idx) = data_mat_idx_list(list_idx) + 1;
+        end
+    end
+end
+
+figure('Name','Interlatency')
+plot(timestamp_mat',time_mat','linewidth',1)
+title('Interlatency','fontsize',FONTSIZE)
+xlabel('time (seconds)','fontsize',FONTSIZE)
+ylabel('time (nanoseconds)','fontsize',FONTSIZE)
+legend(pad_name_list)
 
 fclose('all');
