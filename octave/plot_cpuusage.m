671fc099cc77a0eff1f57dfb8d0483d4f4cd1601
Manuel <manuel.leiva@ridgerun.com>
2016-06-20 11:00:52 -0600

    Add octave plot script for cpuusage event

--- octave/plot_cpuusage.m
+++ octave/plot_cpuusage.m
@@ -4,6 +4,10 @@
 RESULT = 1;
 FONTSIZE = 14;
 LINEWIDTH = 1;
+# CPU_USAGE_AVERAGE = 0 Doesn't displays the average CPU usage.
+# CPU_USAGE_AVERAGE = 1 Displays also the average CPU usage.
+# CPU_USAGE_AVERAGE = 2 Displays only the average CPU usage.
+CPU_USAGE_AVERAGE = 1
 
 # Constants
 TRUE = 1;
@@ -38,38 +42,76 @@ end
 
 printf('Event num %d\n',event_count)
 
+
+# Creata matrix to store the data
+# Use cpu_num + 1 to add the average value
+value_mat = nan(cpu_num + 1,event_count);
+timestamp_mat = nan(cpu_num + 1,event_count);
+
+
 frewind(fileID)
 
 cpu_idx = 1;
 count = 1
-while (count == 1)
+for event_idx = 1 : event_count
+
     [timestamp count] = fscanf(fileID,'[%s]');
-    timestamp
     if (count == 0)
         break
     end
+    [timestamp_array] = sscanf(timestamp,'%d:%d:%f]');
+    timestamp_val = timestamp_array(3) + (timestamp_array(2) * 60) + timestamp_array(1);
+    timestamp_mat(1:end,event_idx) = timestamp_val;
     
-    for cpu_idx = 1 : (cpu_num - 1)
+    for cpu_idx = 2 : (cpu_num)
         [cpu_val, count] = fscanf(fileID,'%f,"');
-        cpu_val
+        value_mat(cpu_idx,event_idx) = cpu_val;
     end
+    # Store the last cpu value
     [cpu_val, count] = fscanf(fileID,'%f\n"');
-    cpu_val
+    value_mat(cpu_num + 1,event_idx) = cpu_val;
     
-    # Creata matrix to store the data
-    %~ timestamp_mat = nan(length(serie_name_list),max(serie_freq_list));
-    %~ value_mat = nan(size(timestamp_mat));
 end
     
 fclose(fileID);
 
-%~ figure('Name','Frame rate')
-%~ plot(timestamp_mat',time_mat','linewidth',LINEWIDTH)
-%~ title('Frame rate','fontsize',FONTSIZE)
-%~ xlabel('time (seconds)','fontsize',FONTSIZE)
-%~ ylabel('Frame per second','fontsize',FONTSIZE)
-%~ legend(element_name_list)
-%~ 
-%~ print tracer -dpdf -append
+
+if (1 == CPU_USAGE_AVERAGE) || (2 == CPU_USAGE_AVERAGE)
+    # Create cpu name list
+    cpu_name_list{1} = 'CPU average';
+    # Compute the average CPU usage
+    value_mat(1,:) = mean(value_mat(2:end,:));
+end
+
+if (0 == CPU_USAGE_AVERAGE) || (1 == CPU_USAGE_AVERAGE)
+    for cpu_idx = 1 : (cpu_num)
+        cpu_name_list{cpu_idx + 1} = sprintf('CPU %d',cpu_idx);
+    end
+end
+
+figure('Name','CPU usage')
+
+
+# Show plot legend
+switch CPU_USAGE_AVERAGE
+    case 0
+        plot(timestamp_mat(2:end,:)',value_mat(2:end,:)','linewidth',LINEWIDTH)
+        legend(cpu_name_list{2:end})
+    case 1
+        plot(timestamp_mat',value_mat','linewidth',LINEWIDTH)
+        legend(cpu_name_list)
+    case 2
+        plot(timestamp_mat(1,:)',value_mat(1,:)','linewidth',LINEWIDTH)
+        legend(cpu_name_list{1})
+   otherwise
+      plot(timestamp_mat',value_mat','linewidth',LINEWIDTH)
+      legend(cpu_name_list)
+end
+
+title('CPU usage','fontsize',FONTSIZE)
+xlabel('time (seconds)','fontsize',FONTSIZE)
+ylabel('Usage (%)','fontsize',FONTSIZE)
+
+print tracer -dpdf -append
 
 
